9989acfa956c3f0391fb580852ac690e6e1b2021
vedantshetti <vedantshetti123456@gmail.com>
2026-03-02 10:54:50 +0530

    updated runtime file

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,35 +10,23 @@ config :todoer_phoenix, TodoerPhoenixWeb.Endpoint,
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+      raise "DATABASE_URL is missing"
 
   config :todoer_phoenix, TodoerPhoenix.Repo,
     url: database_url,
     ssl: true,
-    ssl_opts: [verify: :verify_none],
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+      raise "SECRET_KEY_BASE is missing"
 
   host = System.get_env("PHX_HOST") || "example.com"
 
-  config :todoer_phoenix, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
-
   config :todoer_phoenix, TodoerPhoenixWeb.Endpoint,
+    server: true,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
-    ],
+    http: [port: String.to_integer(System.get_env("PORT") || "4000")],
+    check_origin: ["https://#{host}"],
     secret_key_base: secret_key_base
 end
